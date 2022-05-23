e85a52f73ed2ea48864a96dfef57f7abba503d1f
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-05-23 23:23:14 +0530

    Print alternate elements of an array

---
+++ Arrays/Java/Print_alternate_elements_of_an_array.java
@@ -0,0 +1,10 @@
+class GfG
+{
+    public static void print(int arr[], int n)
+    {
+        // your code here
+        for(int i=0; i<n; i=i+2) {
+            System.out.print(arr[i]+" ");
+       }
+    }
+}
