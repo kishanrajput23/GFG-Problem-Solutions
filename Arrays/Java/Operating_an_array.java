b4b42f1669b6e53c12e186f62a7a046f40b2b6f6
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-05-30 23:52:17 +0530

    Operating an array

---
+++ Arrays/Java/Operating_an_array.java
@@ -0,0 +1,42 @@
+class GfG
+{		
+	public boolean searchEle(int a[],int x)
+       {
+		//add code here.
+		for(int i=0; i<a.length; i++){
+            if (a[i]==x) {
+        return true;
+            }
+		}
+    return false;
+       }
+	public boolean insertEle(int a[],int y,int yi)
+        {
+            //add code here.
+            for (int i=0; i<100; i++) {
+                if (i == yi) {
+                    a[i] = y;
+                    return true;
+                }
+            }
+        return false;
+	}
+	public boolean deleteEle(int a[],int z)
+        {
+		//add code here.
+		int index=0;
+        boolean num=false;
+        for(int i=0; i<100; i++) {
+            if(a[i]==z) {
+                index = i;
+                num=true;
+            }
+        }
+        if(num == true) {
+            for(int i=99; i>index; i--) {
+                a[i-1] = a[i];
+            }
+        }
+        return true;
+	}
+}
