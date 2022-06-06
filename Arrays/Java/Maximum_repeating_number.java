ab742c06ae3a5907b97cc6135a6ed85b9a7d7e07
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-06-06 16:45:03 +0530

    Maximum repeating number

---
+++ Arrays/Java/Maximum_repeating_number.java
@@ -0,0 +1,26 @@
+class Solution {
+    int maxRepeating(int[] arr, int n, int k) {
+        // code here
+        Arrays.sort(arr);
+        int count = 0;
+        int max = 0;
+        int num = -1;
+        for (int i=1; i<n; i++){
+            if (arr[i] == arr[i-1]) {
+                count++;
+            }
+            else {
+                if(count>max){
+                    max = count;
+                    num = arr[i-1];
+                }
+                count=0;
+            }
+        }
+        if (count>max) {
+            max = count;
+            num = arr[n-1];
+        }
+        return num;
+    }
+}
