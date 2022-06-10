79d3bbc7854a4597d59fc362fd4d2df3f78425f5
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-06-10 19:46:23 +0530

    Count of smaller elements

---
+++ Arrays/Java/Count_of_smaller_elements.java
@@ -0,0 +1,14 @@
+class Compute {
+    
+    public long countOfElements(long arr[], long n, long x)
+    {
+        int cnt = 0;
+        for (int i=0; i<n; i++) {
+            if (arr[i] <= x) {
+            cnt += 1;
+            }
+        }
+        return cnt;
+        
+    }
+}
