61de4abf22a271897758cbf268a2345624989e31
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-06-13 18:46:06 +0530

    Remainder with 7

---
+++ Strings/Python/Remainder_with_7.py
@@ -0,0 +1,8 @@
+class Solution:
+    #your task is to complete this function
+    #Function should return the required answer
+    #You are not allowed to convert string to integer
+    def remainderWith7(self, str):
+        #Code here
+        x = int(str)
+        return x%7
