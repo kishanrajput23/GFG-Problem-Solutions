c005bb84695698d7ca1a88381774a6ac1edc5e07
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-07-14 23:33:06 +0530

    Sum of Array Elements

---
+++ Arrays/Java/Sum_of_Array_Elements.java
@@ -0,0 +1,12 @@
+class Get
+{
+    public static int sumElement(int arr[], int n)
+    {
+        // Your code here
+        int sum = 0;
+        for (int i:arr) {
+            sum += i;
+        }
+        return sum ;
+    }
+}
