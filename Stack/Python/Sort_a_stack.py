ec749de271150e4c13db6207d591dbde608228ce
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-07-21 23:39:27 +0530

    Sort a stack

---
+++ Stack/Python/Sort_a_stack.py
@@ -0,0 +1,9 @@
+class Solution:
+    # your task is to complete this function
+    # function sort the stack such that top element is max
+    # funciton should return nothing
+    # s is a stack
+    def sorted(self, s):
+        # Code here
+        s.sort()
+        return s[::-1]
