d7fb226ca9d4b88f959cfb96957fd5e1943d822a
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-07-26 23:42:12 +0530

    Reorganize The Arra

---
+++ Arrays/Java/Reorganize_The_Array.java
@@ -0,0 +1,22 @@
+class Complete{
+    
+   
+    // Function for finding maximum and value pair
+    public static int[] Rearrange (int arr[], int n) {
+        // Complete the function
+        int[] count = new int[n];
+        for(int i = 0; i < n; i++) {
+            if(arr[i] > -1)
+                count[arr[i]]++;
+   	    }
+        for(int i = 0; i < count.length; i++) {
+            if(count[i] > 0) {
+       		    count[i]=i;
+            }
+            else {
+			    count[i] = -1; 
+		    }
+        }
+        return count;
+    }
+}
