33be32fe31a14b60fffcdaba4dc956bac8fd708f
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-08-02 23:38:57 +0530

    Max Value

---
+++ Arrays/Java/Max_Value.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    public static int maxVal(int a[], int n) {
+        int max = Integer.MIN_VALUE, 
+        min = Integer.MAX_VALUE;
+        for (int i=0; i<n; i++) {
+            if ((a[i]-i)>max) {
+                max = a[i] - i;
+            }
+            if ((a[i]-i)<min) {
+                min = a[i] - i;
+            }
+        }
+        return max - min;
+    }
+}
