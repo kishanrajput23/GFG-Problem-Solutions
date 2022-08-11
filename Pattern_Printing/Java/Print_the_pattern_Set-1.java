67c1d3ad5bd8f95f53f249b63e2b395ffe5d48ab
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-08-11 23:26:30 +0530

    Print the pattern | Set-1

--- Pattern_Printing/Java/Print_the_pattern_Set-1.java
+++ Pattern_Printing/Java/Print_the_pattern_Set-1.java
@@ -3,7 +3,7 @@ class GfG {
         // Your code here
         for (int k=0; k<n; k++) {
             for (int i=n; i>=1; i--) {
-                for (int j=1; j<= n-k; j++) {
+                for (int j=1; j<=n-k; j++) {
                     System.out.print(i + " ");
                }
             }
