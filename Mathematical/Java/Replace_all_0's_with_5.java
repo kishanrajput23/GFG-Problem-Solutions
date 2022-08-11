5ecacdead7be3166e757045e90de651c94a0f61a
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2022-08-11 23:40:56 +0530

    Replace all 0's with 5

---
+++ Mathematical/Java/Replace_all_0's_with_5.java
@@ -0,0 +1,8 @@
+/*Complete the function below*/
+class Solution {
+	
+	public static int convertFive(int n) {
+        //add code here.
+        return Integer.valueOf(String.valueOf(n).replace('0','5'));
+    }
+}
