dbfb70d099274ed5e289f945772e1c178c8776a9
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2022-08-30 19:12:38 +0530

    Remove characters from alphanumeric string

--- Strings/Python/Remove_characters_from_alphanumeric_string.py
+++ Strings/Python/Remove_characters_from_alphanumeric_string.py
@@ -1,1 +1,8 @@
-
+class Solution:
+	def removeCharacters(self, S):
+		# code here
+		a=[]
+        for i in S:
+            if (i == '0'or i == '1'or i == '2'or i == '3'or i == '4'or i == '5'or i == '6'or i == '7'or i == '8'or i == "9" ):
+                a.append(i)
+        return "".join(a)
