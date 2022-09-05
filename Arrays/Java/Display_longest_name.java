774d531ea1f433a6132cece43ee0b77964c7d0cd
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2022-09-05 23:45:53 +0530

    Display Longest Name

--- Arrays/Java/Display_longest_name.java
+++ Arrays/Java/Display_longest_name.java
@@ -1,6 +1,6 @@
 class GFG {
     String longest(String names[], int n) {
-        int MAX=0;
+        int MAX = 0;
         String ans="";
         for (int i=0; i<n; i++) {
             if (names[i].length() > MAX) {
