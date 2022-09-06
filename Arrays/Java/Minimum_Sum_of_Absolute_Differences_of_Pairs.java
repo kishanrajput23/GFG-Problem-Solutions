cf3a10fee6292b1fa0ca1ad6e7fac59b4a858738
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2022-09-06 17:04:19 +0530

    Minimum Sum of Absolute Differences of Pairs

--- Arrays/Java/Minimum_Sum_of_Absolute_Differences_of_Pairs.java
+++ Arrays/Java/Minimum_Sum_of_Absolute_Differences_of_Pairs.java
@@ -1,5 +1,5 @@
 class Solution { 
-    long findMinSum(int[] A,int[] B,int N) { 
+    long findMinSum(int[] A, int[] B, int N) { 
         Arrays.sort(A);
         Arrays.sort(B);
         long sum = 0;
