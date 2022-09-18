fdd753b7e72b417484aeeecb0076f80b66130765
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2022-09-18 23:38:34 +0530

    Equal point in a string of brackets

--- Arrays/Java/Equal_point_in_a_string_of_brackets.java
+++ Arrays/Java/Equal_point_in_a_string_of_brackets.java
@@ -1,8 +1,8 @@
 class Solution {
     public long countSub(String str) {
         // Your code goes here
-        int close = 0 ;
-        int i ;
+        int close = 0;
+        int i;
         for (i=0; i<str.length(); i++) {
             if (str.charAt(i) ==')') {
                close++;
