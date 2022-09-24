0ae699604fd352f2d381c8e880877660f923f362
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2022-09-24 23:46:23 +0530

    Update Remove_character.java

--- Strings/Java/Remove_character.java
+++ Strings/Java/Remove_character.java
@@ -2,7 +2,7 @@ class Solution{
     static String removeChars(String string1, String string2){
         // code here
         String s = "";
-        for (int i = 0; i<string1.length(); i++) {
+        for (int i=0; i<string1.length(); i++) {
             if (!string2.contains(String.valueOf(string1.charAt(i)))) {
               s = s + string1.charAt(i);
             }
