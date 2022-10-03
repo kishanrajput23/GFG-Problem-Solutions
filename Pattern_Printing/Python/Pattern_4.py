e51a4243bf618a124b70f84e92e85661ae421474
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2022-10-03 23:32:43 +0530

    Pattern 4

---
+++ Pattern_Printing/Python/Pattern_4.py
@@ -0,0 +1,7 @@
+class Solution:
+    def printTriangle(self, N):
+        # Code here
+        for i in range(1, N+1):
+            for j in range(1, i+1):
+                print(i, end=" ")
+            print()
