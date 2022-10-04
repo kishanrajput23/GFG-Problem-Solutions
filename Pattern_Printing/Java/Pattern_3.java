d4afab04fbf1e8fe1bb35c8aa361316bdd06c0ac
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2022-10-04 23:34:18 +0530

    Pattern 3

---
+++ Pattern_Printing/Java/Pattern_3.java
@@ -0,0 +1,12 @@
+class Solution {
+
+    void printTriangle(int n) {
+        // code here
+        for (int i=1; i<n+1; i++) {
+            for (int j=1; j<=i; j++) {
+               System.out.print(j +" ");
+            }
+        System.out.println();
+        }
+    }
+}
