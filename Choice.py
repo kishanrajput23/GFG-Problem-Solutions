b31482991d1a0e983eddb641c8a16ec275c7ee81
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2022-10-05 23:29:44 +0530

    Choice

---
+++ Choice.py
@@ -0,0 +1,7 @@
+class Solution:
+    def arrayForm(self, a, b, c):
+        # Code here
+        return [a, b, c]
+    def stringForm(self, a, b, c):
+        # Code here
+        return str(a) + str(b) + str(c)
