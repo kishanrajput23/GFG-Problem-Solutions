dd1fa18c7726d3713727fe8e6fbe0388d71e72ea
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2022-10-06 23:45:28 +0530

    BigInteger Multiply

---
+++ BigInteger_Multiply.java
@@ -0,0 +1,10 @@
+class MathematicalOperation{
+    
+    static BigInteger mul(BigInteger x, BigInteger y){
+    
+        // Your code here
+        return x.multiply(y);
+             
+    }
+    
+}
