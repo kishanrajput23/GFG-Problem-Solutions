76dba5293e65c7eac3e0a4687f13a506ce704633
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2022-10-12 23:55:02 +0530

    Find the number of subarrays having even sum

--- Arrays/Java/Find_the_number_of_subarrays_having_even_sum.java
+++ Arrays/Java/Find_the_number_of_subarrays_having_even_sum.java
@@ -5,7 +5,7 @@ class Solution {
        int odd=0;
        int sum=0;
        int ans=0;
-       for(int i=0;i<n;i++){
+       for (int i=0;i<n;i++) {
            sum+=arr[i];
            if(sum%2==0){
                ans+=1+even;
