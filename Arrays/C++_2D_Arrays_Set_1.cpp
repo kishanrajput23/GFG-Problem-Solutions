e8882c3ec6dda3f531a4264d99148951d7aa3572
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2022-10-18 23:57:22 +0530

    C++ 2D Array Set I

--- Arrays/C++_2D_Arrays_Set_1.cpp
+++ Arrays/C++_2D_Arrays_Set_1.cpp
@@ -1,5 +1,4 @@
-vector<vector<int>> transpose(int a[][M], int n)
-{
+vector<vector<int>> transpose(int a[][M], int n) {
     // Code here
     vector<vector<int>> arr(n, vector<int>(n));
     for (int i = 0; i < n; i++) {
