179672ffd3688ab6e8b6bcd1d892d5f24d135cc5
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2022-11-24 20:39:14 +0530

    Delete Array

---
+++ CPP/Delete_Array.cpp
@@ -0,0 +1,4 @@
+// Code to delete the array
+// Task is to comment the line which deletes 
+// the array such that required output is printed
+//delete[] arr; //comment this line so memory doesn't get deallocated
