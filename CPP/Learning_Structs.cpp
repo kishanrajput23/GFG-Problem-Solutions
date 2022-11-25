89626938eab8424aa171d7c93f03ddccb69933c8
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2022-11-25 23:40:23 +0530

    Learning Structs

---
+++ CPP/Learning_Structs.cpp
@@ -0,0 +1,11 @@
+/*structure node of linked list
+  to store data and link 
+  to the next node
+*/
+struct Node
+{
+    //Your code here
+    //Linked list has data and next members
+    int data;
+    Node*next;
+};
