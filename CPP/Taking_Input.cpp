aa645c2d7573181f503afe43df37b202a0112f3e
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2022-12-08 22:27:03 +0530

    Taking Input

---
+++ CPP/Taking_Input.cpp
@@ -0,0 +1,9 @@
+void inputData()
+{
+       int a;
+       string b;
+       //Use cin to take input and cout a and b with a space between them. 
+       //You don't have to print any new lines after cout 
+       cin>>a>>b;
+       cout<<a<<" " <<b;
+}
