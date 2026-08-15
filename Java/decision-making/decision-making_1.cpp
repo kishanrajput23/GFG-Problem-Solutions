f62b60e86d829cf94df19aee8e0ea410c9f14502
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2026-08-15 13:03:30 +0530

    Added Decision Making solution - CodeHub Sync

---
+++ Java/decision-making/decision-making_1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    // code here
+    if (n<m) {
+        cout<< "less";
+    }
+    else if (n==m) {
+        cout<< "equal";
+    }
+    else {
+        cout<< "greater";
+    }
+
+    return 0;
+}
