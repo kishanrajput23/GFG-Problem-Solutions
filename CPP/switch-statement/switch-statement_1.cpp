cba8114a32a2d556316966126183612e3fbe1200
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2026-08-17 22:11:45 +0530

    Added Switch Statement solution - CodeHub Sync

---
+++ CPP/switch-statement/switch-statement_1.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    // code here
+    
+    switch(n){
+        case 1:
+            cout<< "One\n";
+            break;
+        case 2:
+            cout<< "Two\n";
+            break;
+        case 3:
+            cout<< "Three\n";
+            break;
+        case 4:
+            cout<< "Four\n";
+            break;
+        case 5:
+            cout<< "Five\n";
+            break;
+        case 6:
+            cout<< "Six\n";
+            break;
+        case 7:
+            cout<< "Seven\n";
+            break;
+        case 8:
+            cout<< "Eight\n";
+            break;
+        case 9:
+            cout<< "Nine\n";
+            break;
+        default:
+            cout<< "Unknown\n";
+            break;
+    }
+    
+    return 0;
+}
