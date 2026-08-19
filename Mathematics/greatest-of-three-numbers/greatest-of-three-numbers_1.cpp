b62b612051cf48a8184648056a11191293431910
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2026-08-19 22:58:01 +0530

    Added Greatest of three numbers solution - CodeHub Sync

---
+++ Mathematics/greatest-of-three-numbers/greatest-of-three-numbers_1.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+
+    // code here
+    cout<< max(a, max(b, c));
+
+    return 0;
+}
