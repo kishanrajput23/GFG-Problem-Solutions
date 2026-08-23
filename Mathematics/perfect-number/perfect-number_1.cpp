55ae9c3db03343ec5f651dc11f05bba783cf23ed
Kishan Kumar Rai <kishan.rai99693@gmail.com>
2026-08-23 14:00:25 +0530

    Added Perfect Number solution - CodeHub Sync

---
+++ Mathematics/perfect-number/perfect-number_1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+	public:
+	int fact(int n) {
+		if (n == 0) {
+			return 1;
+		}
+		
+		return n * fact(n - 1);
+	}
+	
+	int isPerfect(int N) {
+		// code here
+		int m = N;
+		int sum = 0;
+		
+		while (N != 0) {
+			int k = N % 10;
+			sum = fact(k) + sum;
+			N = N/10;
+		}
+		
+		if (sum == m) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+	}
+};
